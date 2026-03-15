6516cf25b18296dc58af714145d787e88d84e98d
Weej-cyber <cww3shopping@gmail.com>
2026-03-15 18:02:43 -0400

    Create supabase.js

---
+++ src/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://rwauwkrdzcesyhwpaeow.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3YXV3a3JkemNlc3lod3BhZW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDgyMjcsImV4cCI6MjA4NTM4NDIyN30.kzLnCqdOcbXtMXJquxAzxwMlVhlFcYh1y2O5m8Uw1zs'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
