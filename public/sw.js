a9e1eda1516fc8036a05332ab0792fe94a7229c4
Weej-cyber <cww3shopping@gmail.com>
2026-03-17 13:58:21 +0000

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'fetchus-v2'
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/paw.svg',
+  '/paw-192.png',
+  '/paw-512.png',
+  '/paw-180.png',
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).catch(() => caches.match('/index.html'))
+    })
+  )
+})
